8721ea3dd2ef496d67b0f5426596fd64150a7e58
Milka Ferezliyska <mileto94@abv.bg>
2017-01-06 20:52:59 +0200

    Add CORS settings

--- vkusotiiki/settings/base_settings.py
+++ vkusotiiki/settings/base_settings.py
@@ -44,15 +44,23 @@ INSTALLED_APPS = [
     'filer',
     'mptt',
     'firebase',
+    'corsheaders',
 
     # custom
     'vkusotiiki',
     'recipes',
 ]
 
+CORS_ORIGIN_WHITELIST = (
+    'vkusotiiki-bg.firebase.com',
+    'localhost:8000',
+    '127.0.0.1:9000'
+)
+
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
